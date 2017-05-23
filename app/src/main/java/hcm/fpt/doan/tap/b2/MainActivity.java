a44e5252aac3b3c6379f32f53741e4fda437483b
ThuanLCSE <thuanlc.devil@gmail.com>
2017-05-23 20:19:02 +0700

    Sms update

--- app/src/main/java/hcm/fpt/doan/tap/b2/MainActivity.java
+++ app/src/main/java/hcm/fpt/doan/tap/b2/MainActivity.java
@@ -1,9 +1,12 @@
 package hcm.fpt.doan.tap.b2;
 
+import android.Manifest;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 
@@ -29,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
                 Uri uri = Uri.parse("tel:01246789111");
                 Intent i = new Intent(Intent.ACTION_CALL, uri);
                 startActivity(i);
+            }
+        });
+        Button btnSms = (Button) findViewById(R.id.button3) ;
+        btnSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNo = "01224402799";
+                String message = "A hi hi ";
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(phoneNo, null, message, null, null);
             }
         });
     }
